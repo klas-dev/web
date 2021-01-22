6b9de87ad7bb55b0ccadd3518dff39c7d7761e62
root <root@Charliee.klas.com>
2021-01-22 13:49:22 -0300

    cambios textos

--- src/componentes/denthist.jsx
+++ src/componentes/denthist.jsx
@@ -7,9 +7,9 @@ function Denthist(){
             <img className="w-3/5 md:w-4/12" src="./denthistlogo.png" alt="logo de denthist" />
             </div>
             <div className="w-8/12 md:w-2/5 flex flex-col items-center md:items-center justify-center">
-            <h1 className="m-2 w-full text-center text-3xl font-bold">DentHist</h1>
+            <h1 className="m-2 w-full text-center text-3xl md:text-4xl font-bold">DentHist</h1>
             <p className="m-2 text-sm md:text-lg text-center mb-4">Herramienta administrativa orientada a odontólogos, que lleva un historial clínico dental de los pacientes y un fácil acceso a sus medios de contacto.</p>
-            <Link to="/denthist" className="m-auto md:text-lg p-2 border-black border-4 rounded cursor-pointer">Ver más</Link>
+            <Link to="/denthist" className="m-auto font-bold md:text-xl p-2 border-black border-4 rounded cursor-pointer">Ver más</Link>
             </div>
         </div>
         </div>
--- src/componentes/paw5.jsx
+++ src/componentes/paw5.jsx
@@ -10,9 +10,9 @@ function Paw(){
                     <img src="./paw5logo.png" className="w-11/12 md:w-2/5" alt="logo de paw5" /> 
                 </div>
                 <div className=" w-8/12 md:w-2/5 flex flex-col items-center md:items-start justify-center">
-                <h1 className="m-2 w-full text-center text-3xl font-bold">Paw5</h1>
+                <h1 className="m-2 w-full text-center text-3xl md:text-4xl font-bold">Paw5</h1>
                 <p className="m-2 text-sm md:text-lg text-center mb-4">Sistema de asistencia clínica para veterinarias, que lleva un registro clinico de los animales y permite un contacto directo con sus respectivos dueños.</p> 
-                <Link to="/paw5" className="m-auto md:text-lg p-2 border-black border-4 rounded cursor-pointer">Ver más</Link>
+                <Link to="/paw5" className="m-auto font-bold md:text-xl p-2 border-black border-4 rounded cursor-pointer">Ver más</Link>
             </div>
             </div>
         </div>
