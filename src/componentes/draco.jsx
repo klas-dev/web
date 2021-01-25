abd6b0c735b6d21857a83a4e379d763fa0c1ab14
root <root@Charliee.klas.com>
2021-01-25 20:12:48 -0300

    acomodar textos para pantallas muy chicas

--- src/componentes/draco.jsx
+++ src/componentes/draco.jsx
@@ -8,7 +8,7 @@ function Draco(){
                 </div>
                 <div className="w-8/12 md:w-2/5 flex flex-col items-center md:items-start justify-center">
                     <h1 className="m-2 w-full text-center font-bold text-3xl md:text-4xl">Draco</h1>
-                    <div className="m-2 mb-4 text-sm text-center md:text-lg" >Sistema de administración y organización comercial completamente adaptable basado en los requisitos de tu negocio, pensado para facilitar solucionar los problemas administrativos mas habituales y diseñado para aumentar el rendimiento de tu emprendimiento.</div>
+                    <div className="m-2 mb-4 text-xs sm:text-sm text-center md:text-lg" >Sistema de administración y organización comercial completamente adaptable basado en los requisitos de tu negocio, pensado para facilitar solucionar los problemas administrativos mas habituales y diseñado para aumentar el rendimiento de tu emprendimiento.</div>
                     <Link to="/draco" className="m-auto font-bold p-2 md:text-xl rounded border-black border-4 cursor-pointer">Ver más</Link>
                 </div>
             </div>
